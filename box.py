3c5fe4d529d8e7848cec8e573bb3a89d805ef0e7
Kiri <69686396+Kkiriya@users.noreply.github.com>
2024-12-16 17:26:49 -0500

    added part of constructor (not finished)

--- box.py
+++ box.py
@@ -1,7 +1,13 @@
 class box:
     """List all of the items contained within it as well as a short description, a category, the date of the last update, and a id and barcode"""
-    
-    itemsList = [""] #list of all items in the box
-    boxDescription = "" #short description of the box (max 30 char)
-    category = "" #categorie (i.e useful, useless, clothes, books, trinkets, cables, etc) (max 15 char)
-    lastUpdate ="" #last date the box has been updated
+    __nbrId = 0
+    __barcode = 0
+    __itemsList = [""] #list of all items in the box
+    __boxDescription = "" #short description of the box (max 30 char)
+    __category = "" #categorie (i.e useful, useless, clothes, books, trinkets, cables, etc) (max 15 char)
+    __lastUpdate ="" #last date the box has been updated
+
+def __init__(self, itemsList, boxDescription, category):
+    self.__itemsList
+    self.__boxDescription
+    self.category
