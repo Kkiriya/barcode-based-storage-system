0ef0e3349f74cc5dc1e133624da84ac60e3b708c
Kiri <69686396+Kkiriya@users.noreply.github.com>
2024-12-16 16:58:24 -0500

    Create box.py

---
+++ box.py
@@ -0,0 +1,7 @@
+class box:
+    """List all of the items contained within it as well as a short description, a category, the date of the last update, and a id and barcode"""
+    
+    itemsList = [""] #list of all items in the box
+    boxDescription = "" #short description of the box (max 30 char)
+    category = "" #categorie (i.e useful, useless, clothes, books, trinkets, cables, etc) (max 15 char)
+    lastUpdate ="" #last date the box has been updated
